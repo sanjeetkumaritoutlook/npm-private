1385df8c8ce7db3fdf11f61ca7e7002f5ad1b9c8
unknown <sanjeetkumarit@outlook.com>
2025-03-17 09:21:35 +0530

    basic package

---
+++ README.md
@@ -0,0 +1,3 @@
+# My First NPM Package 🎯
+
+This is a basic npm package that greets people.
---
+++ index.js
@@ -0,0 +1,7 @@
+// index.js
+
+function greet(name) {
+  return `Hello, ${name}! 🌟`;
+}
+
+module.exports = { greet };
